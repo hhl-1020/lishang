4b692f81e134cd1ade6e4656c7cf6f1d4027e2d8
hhl <3246443700@qq.com>
2020-04-14 17:14:32 +0800

    Initial commit

---
+++ lishang/src/main/java/com/lishang/LishangApplication.java
@@ -0,0 +1,13 @@
+package com.lishang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LishangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LishangApplication.class, args);
+    }
+
+}
